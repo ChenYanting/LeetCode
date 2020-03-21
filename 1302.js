2aed94899967a3eaede87edc34ebaf1a31ef4ab5
Yanting Chen <mx.alexender@gmail.com>
2020-03-21 12:14:12 +0700

    Deepest Leaves Sum

---
+++ 1302.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var deepestLeavesSum = function(root) {
+    let deepest = 0;
+    const leaves = new Map();
+    const traversal = (node, level) => {
+        if (node === null) return
+        if (node.left === null && node.right === null) {
+            if (level >= deepest) {
+                deepest = Math.max(level, deepest);
+                if (leaves.has(level)) leaves.set(level, [...leaves.get(level), node.val]);
+                else leaves.set(level, [node.val]);
+            }
+            return;
+        }
+
+        traversal(node.left, level + 1);
+        traversal(node.right, level + 1);
+    }
+
+    traversal(root, 1);
+    return leaves.get(deepest).reduce((acc, cur) => acc + cur);
+};

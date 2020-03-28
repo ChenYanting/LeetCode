3bc1d1a6a117856585434cd9f3e4240c265cbb9e
Yanting Chen <mx.alexender@gmail.com>
2020-03-28 15:06:59 +0700

    Delete Duplicate Emails

---
+++ easy/196.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+DELETE p1
+FROM Person p1, Person p2
+WHERE p1.Email = p2.Email AND p1.Id > p2.Id;
+
+Person
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
